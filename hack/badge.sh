2b88b06e053c4cac3a98c144c6f054e3942c72d2
Shiming Zhang <wzshiming@foxmail.com>
2021-10-09 11:06:58 +0800

    Add badge.sh

---
+++ hack/badge.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+LOGFILE="./sync.log"
+sync="$(cat "${LOGFILE}" | grep " SYNCHRONIZED: " | wc -l | tr -d ' ' || :)"
+unsync="$(cat "${LOGFILE}" | grep " NOT-SYNCHRONIZED: " | wc -l | tr -d ' ' || :)"
+sum=$(($sync + $unsync))
+
+if [[ "${QUICKLY}" == "true" ]]; then
+    echo "https://img.shields.io/badge/Sync-${sync}%2F${sum}-blue"
+    wget "https://img.shields.io/badge/Sync-${sync}%2F${sum}-blue" -O badge.svg
+else
+    echo "https://img.shields.io/badge/Deep%20Sync-${sync}%2F${sum}-blue"
+    wget "https://img.shields.io/badge/Deep%20Sync-${sync}%2F${sum}-blue" -O badge.svg
+fi
--- hack/check.sh
+++ hack/check.sh
@@ -41,15 +41,3 @@ for domain in "${!DOMAIN_MAP[@]}"; do
         }
     done
 done
-
-sync="$(cat "${LOGFILE}" | grep " SYNCHRONIZED: " | wc -l | tr -d ' ' || :)"
-unsync="$(cat "${LOGFILE}" | grep " NOT-SYNCHRONIZED: " | wc -l | tr -d ' ' || :)"
-sum=$(($sync + $unsync))
-
-if [[ "${QUICKLY}" == "true" ]]; then
-    echo "https://img.shields.io/badge/Sync-${sync}%2F${sum}-blue"
-    wget "https://img.shields.io/badge/Sync-${sync}%2F${sum}-blue" -O badge.svg
-else
-    echo "https://img.shields.io/badge/Deep%20Sync-${sync}%2F${sum}-blue"
-    wget "https://img.shields.io/badge/Deep%20Sync-${sync}%2F${sum}-blue" -O badge.svg
-fi
